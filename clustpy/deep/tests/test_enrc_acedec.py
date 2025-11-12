31db62fc3090b8d9c15fcb3fd5f34d9e30a20c06
Collin Leiber <collin.leiber@aalto.fi>
2025-11-12 10:24:03 +0200

    fix failing enrc and acedec test

--- clustpy/deep/tests/test_enrc_acedec.py
+++ clustpy/deep/tests/test_enrc_acedec.py
@@ -8,46 +8,46 @@ from clustpy.deep.tests._helpers_for_tests import _test_dc_algorithm_simple, _te
 @pytest.mark.skip(reason="Checks are not completly implemented yet")
 def test_acedec_estimator():
     # Ignore check_methods_subset_invariance due to numerical issues
-    check_clustpy_estimator(ACeDeC(3, pretrain_epochs=3, clustering_epochs=3, init_subsample_size=15),
+    check_clustpy_estimator(ACeDeC(3, pretrain_epochs=3, clustering_epochs=3, init_subsample_size=30),
                             ("check_complex_data", "check_methods_subset_invariance"))
 
 
 @pytest.mark.skip(reason="Checks are not completly implemented yet")
 def test_enrc_estimator():
     # Ignore check_methods_subset_invariance due to numerical issues
-    check_clustpy_estimator(ENRC([3, 3], pretrain_epochs=3, clustering_epochs=3, init_subsample_size=15),
+    check_clustpy_estimator(ENRC([3, 3], pretrain_epochs=3, clustering_epochs=3, init_subsample_size=30),
                             ("check_complex_data", "check_methods_subset_invariance"))
 
 
 def test_simple_enrc():
-    enrc = ENRC([3, 3], init_subsample_size=15, debug=True)
+    enrc = ENRC([3, 3], init_subsample_size=30, debug=True)
     enrc, _ = _test_dc_algorithm_simple(enrc, check_random_state=False, use_nr_data=True)
     # Standard mechanism for checking random state is not working as the orthogonal matrix V is only approximated
-    enrc2 = ENRC([3, 3], init_subsample_size=15)
+    enrc2 = ENRC([3, 3], init_subsample_size=30)
     enrc2, _ = _test_dc_algorithm_simple(enrc2, check_random_state=False, use_nr_data=True)
     assert np.array_equal(enrc.labels_, enrc.labels_)
     for i in range(len(enrc.cluster_centers_)):
         assert np.array_equal(enrc.cluster_centers_[i], enrc2.cluster_centers_[i])
     # Test if sgd as init is working
-    enrc = ENRC([3, 3], init_subsample_size=15, init="sgd")
+    enrc = ENRC([3, 3], init_subsample_size=30, init="sgd")
     enrc, _ = _test_dc_algorithm_simple(enrc, check_random_state=False, use_nr_data=True)
 
 
 def test_simple_acedec():
-    acedec = ACeDeC(3, init_subsample_size=15, debug=True)
+    acedec = ACeDeC(3, init_subsample_size=30, debug=True)
     acedec, _ = _test_dc_algorithm_simple(acedec, check_random_state=False)
     # Standard mechanism for checking random state is not working as the orthogonal matrix V is only approximated
-    acedec2 = ACeDeC(3, init_subsample_size=15)
+    acedec2 = ACeDeC(3, init_subsample_size=30)
     acedec2, _ = _test_dc_algorithm_simple(acedec2, check_random_state=False)
     assert np.array_equal(acedec.labels_, acedec2.labels_)
     assert np.array_equal(acedec.cluster_centers_[0], acedec2.cluster_centers_[0])
 
 
 def test_acedec_augmentation():
-    acedec = ACeDeC(10, init_subsample_size=15)
+    acedec = ACeDeC(10, init_subsample_size=30)
     _test_dc_algorithm_with_augmentation(acedec)
 
 
 def test_enrc_augmentation():
-    enrc = ENRC([5, 2], init_subsample_size=15)
+    enrc = ENRC([5, 2], init_subsample_size=30)
     _test_dc_algorithm_with_augmentation(enrc, use_nr_data=True)
